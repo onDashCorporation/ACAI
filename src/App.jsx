a6ffd89e950520167caf6f1c70c3ebf618b94768
Arthur <132010817+oArths@users.noreply.github.com>
2024-03-11 01:40:53 -0300

    Fix: home Login laout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css'
-import '@radix-ui/themes/styles.css';
-import {theme } from "@radix-ui/themes"
+import { GlobalStyle } from "./Global";
 
-// 2 - reproveitamento de estrutura 
+// 2 - reproveitamento de estrutura
 // ja que os caminhos vão ser todos renderizados apartir de outelet
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
 function App() {
-
-
   return (
-    <div className='App'>
-      <theme>
-      <Outlet/>
-      </theme>
+    <div className="Global">
+      <GlobalStyle/>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
