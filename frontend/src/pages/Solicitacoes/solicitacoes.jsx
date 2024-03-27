98eff55255ae7fb1742014c4a1e9839adf1c644b
Arthur <arthurferreira1417@gmail.com>
2024-03-26 22:02:16 -0300

    Feat: new desing table and filter functional

--- frontend/src/pages/Solicitacoes/solicitacoes.jsx
+++ frontend/src/pages/Solicitacoes/solicitacoes.jsx
@@ -2,10 +2,13 @@ import * as S from "./style";
 import Nav from "../../components/nav/nav";
 import Header from "../../components/header/header";
 import Dropdown from "../../components/dropdown/dropdown";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Search from "../../components/search/search";
-import Plus from "../../assets/images/plus.svg"
+import Folder from "../../assets/images/folder.svg"
 import data from "./tabledb.json";
+import Pagination from "../../components/pagination/pagination"
+
+
 const solicitacoes = () => {
   
   const [filterop, setFilterop] = useState("Filtro");
@@ -23,11 +26,21 @@ const solicitacoes = () => {
     
     const normalizedSearch = normalizeString(searchValue);
         const newFilteredData = data.filter((item) => {
-          if (filterop === "address") {
-            // Filtro por Departamento
-            return normalizeString(item.address.toString()).includes(normalizedSearch);
+          if (filterop === "Departamento") {
+            return normalizeString(item.departamento.toString()).includes(normalizedSearch);
+          } 
+           else if (filterop === "Nome") {         
+            return normalizeString(item.nome.toString()).includes(normalizedSearch);
+          }
+           else if (filterop === "Status") {          
+            return normalizeString(item.status.toString()).includes(normalizedSearch);
+          } 
+           else if (filterop === "Id") {         
+            return normalizeString(item.id.toString()).includes(normalizedSearch);
+          } 
+           else if (filterop === "Data") {
+            return normalizeString(item.data.toString()).includes(normalizedSearch);
           } else {
-            // Filtro genérico em todas as colunas
             return Object.values(item).some((value) =>
               normalizeString(value.toString()).includes(normalizedSearch)
             );
@@ -36,7 +49,14 @@ const solicitacoes = () => {
         setFilteredData(newFilteredData);
       };
  
- 
+const generateStyles = (key, value) => {
+  switch (key) {
+    case 'status':
+      return value === 'novo' ? 'color: #276e27;'  : value === 'lido' ? 'color: #ccdd32;' : 'color: #6e2d27;';
+    default:
+      return '';
+  }
+};
   return (
     <S.Body>
       <Header />
@@ -77,18 +97,23 @@ const solicitacoes = () => {
               </S.TableHeader>
               <S.TableBody>
               {filteredData.map((item, index) => (
-                <S.TrBody key={index}>
-                  {Object.values(item).map((value, index) => (
-                    <S.TdBody  key={index}>{value}</S.TdBody>
-                  ))}
-                  <S.TdBody>
-                    <S.ImageCell src={Plus} alt="Imagem" onClick={() => handleImageClick(item.id)} />
-                  </S.TdBody>
-                </S.TrBody>
-              ))}
+              <S.TrBody key={index}>
+                {Object.entries(item).map(([key, value], index) => (
+                  <S.StyledTableCell key={index} styles={generateStyles(key, value)}>{value}</S.StyledTableCell>
+                ))}
+                <S.StyledTableCell>
+                   <S.ImageCell src={Folder} alt="Imagem" onClick={() => handleImageClick(item.id)} />
+                </S.StyledTableCell>
+                
+              </S.TrBody>
+            ))}
+            
               </S.TableBody>
+              <S.PaginationConatiner>
+               <Pagination/>
+            </S.PaginationConatiner>
+              
             </S.StyledTable>
-          
             
           </S.TableContainer>
         </S.Container>
